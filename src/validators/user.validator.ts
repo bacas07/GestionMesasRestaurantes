fa9f2a415796e9fe99ebae7ad62b5909672c5231
Mateo Baca <mateobacas07@gmail.com>
2025-05-18 18:40:28 -0500

    feat: Creacion de validadores para Users en user.validator.ts

---
+++ src/validators/user.validator.ts
@@ -0,0 +1,15 @@
+import { object, string, number, array } from 'valibot';
+
+const ReservationSchema = object({
+  table: string(),
+  date: string(),
+  hour: string(),
+  people: number(),
+});
+
+export const UserSchema = object({
+  name: string(),
+  email: string(),
+  number: number(),
+  history: array(ReservationSchema),
+});
