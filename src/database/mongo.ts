a9ec39ff4feeab4f24ff8514658a31969862c4b4
Mateo Baca <mateobacas07@gmail.com>
2025-05-18 09:54:13 -0500

    feat: Conexion a la base de dato hecha

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,13 +1,19 @@
 import { connect } from 'mongoose';
 import { config } from 'dotenv';
+import ApiError from '../errors/apiError.js';
+
+config();
 
 const MONGO_URL: string = process.env.MONGO_URL || '';
 
 export const connectDB = async () => {
   try {
     await connect(MONGO_URL);
-    console.log('> Servidor conectado a la base de datos')
+    console.log('> Servidor conectado a la base de datos');
   } catch (error) {
-    
+    throw new ApiError(
+      `Error connecting to database: ${(error as Error).message}`,
+      500
+    );
   }
 };
