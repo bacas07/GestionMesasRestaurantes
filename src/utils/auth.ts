c22af11a7397f9902f141c3cc8d15ebf2b4e0967
Mateo Baca <mateobacas07@gmail.com>
2025-05-19 09:00:06 -0500

    feat: Agregando creacion y validacion de tokens con JWT en auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,56 @@
-import { Request } from "express";
-import { sign, verify } from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express';
+import { sign, verify } from 'jsonwebtoken';
+import { IUserMongoose } from '../types/types';
+import { config } from 'dotenv';
+import ApiError from '../errors/apiError';
 
-export const generateUserToken = (user: Request) => {
-  const payload = {
-    
+config();
+
+export const generateUserToken = (user: IUserMongoose) => {
+  const secret = process.env.JWT_SECRET_KEY;
+
+  if (!secret) {
+    throw new ApiError('JWT_SECRET_KEY no esta definida', 500);
   }
-}
+
+  const payload = {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+  try {
+    return sign(payload, secret, { expiresIn: '1h' });
+  } catch (error) {
+    throw new ApiError(
+      `Error generando Token -> ${(error as Error).message}`,
+      500
+    );
+  }
+};
+
+export const verifyUserToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const secret = process.env.JWT_SECRET_KEY;
+
+  if (!secret) {
+    throw new ApiError('JWT_SECRET_KEY no esta definida', 500);
+  }
+
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) {
+    throw new ApiError('Token Required', 401);
+  }
+
+  try {
+    const decoded = verify(token, secret);
+    res.locals.user = decoded;
+    next();
+  } catch (error) {
+    throw new ApiError(`Token invalido -> ${error}`, 401);
+  }
+};
